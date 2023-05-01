1e1f47089dccda6777c1c04fa0a0a9ef6591a664
lorocks <63993526+lorocks@users.noreply.github.com>
2023-05-01 18:50:34 +0400

    Add files via upload

--- ONNX/serial_object_detection.py.py
+++ ONNX/serial_object_detection.py.py
@@ -27,55 +27,59 @@ scores = []
 arduino.write(bytes(num, 'utf-8'))
 
 while cap.isOpened():
-    t1 = cv2.getTickCount()
+    try:
+        t1 = cv2.getTickCount()
 
-    if len(scores) > 0 and num == '1':
-        num = '2'
-        arduino.write(bytes(num, 'utf-8'))
-
-        if boxes[0][0] > 5 and boxes[0][1] > 5 and boxes[0][2] < frame.shape[1] - 5 and boxes[0][3] < frame.shape[0] - 5:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            string = base64.b64encode(buffer).decode('utf-8')
-            my_img = {
-                'image': string,
-                'number': 5,
-                'secret': theSecret,
-            }
-
-            # Send frame to Backend
-            res = requests.post('https://ll753-flaskmlbackendlpr.hf.space/sendlicenseY', json=my_img)
-            res.raise_for_status()
-            print(res.content)
-            if not 'Image' in res.content:
-                arduino.write(bytes('3', 'utf-8'))
-                num = '1'
-
-        elif num == '2' and len(scores) == 0:
-            num = '1'
+        if len(scores) > 0 and ( num == '1' or num == '2' ):
+            num = '2'
             arduino.write(bytes(num, 'utf-8'))
 
-    # Read frame from video
-    ret, frame = cap.read()
+            if boxes[0][0] > 5 and boxes[0][1] > 5 and boxes[0][2] < frame.shape[1] - 5 and boxes[0][3] < frame.shape[0] - 5:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                string = base64.b64encode(buffer).decode('utf-8')
+                my_img = {
+                    'image': string,
+                    'number': 5,
+                    'secret': theSecret,
+                }
 
-    if not ret:
-        break
+                # Send frame to Backend
+                res = requests.post('https://ll753-flaskmlbackendlpr.hf.space/sendlicenseY', json=my_img)
+                res.raise_for_status()
+                print(res.content)
+                num = '3'
+                if not 'Image' in res.content:
+                    arduino.write(bytes('3', 'utf-8'))
+                    num = '1'
 
-    # Update object localizer
-    boxes, scores, class_ids = yolov7_detector(frame)
-    # print(boxes, scores, class_ids)
+            elif num == '2' and len(scores) == 0:
+                num = '1'
+                arduino.write(bytes(num, 'utf-8'))
 
-    combined_img = yolov7_detector.draw_detections(frame)
-    cv2.putText(combined_img, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX,
-                1, (255, 255, 0), 2,
-                cv2.LINE_AA)
-    cv2.imshow("Detected Objects", combined_img)
-    t2 = cv2.getTickCount()
-    time1 = (t2 - t1) / freq
-    frame_rate_calc = 1 / time1
+        # Read frame from video
+        ret, frame = cap.read()
 
-    # Press key q to stop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if not ret:
+            break
+
+        # Update object localizer
+        boxes, scores, class_ids = yolov7_detector(frame)
+        # print(boxes, scores, class_ids)
+
+        combined_img = yolov7_detector.draw_detections(frame)
+        cv2.putText(combined_img, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                    1, (255, 255, 0), 2,
+                    cv2.LINE_AA)
+        cv2.imshow("Detected Objects", combined_img)
+        t2 = cv2.getTickCount()
+        time1 = (t2 - t1) / freq
+        frame_rate_calc = 1 / time1
+
+        # Press key q to stop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    except:
+        continue
 
 cap.release()
 cv2.destroyAllWindows()
